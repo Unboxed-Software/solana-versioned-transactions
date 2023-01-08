a973833aa1344ce1c84a2246d8b52a4d21963740
john <johnzhengyuliu@gmail.com>
2023-01-07 20:57:04 -0600

    test lut

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,43 @@ async function main() {
     .getLatestBlockhash()
     .then((res) => res.blockhash)
 
-  const instructions = [
-    web3.SystemProgram.transfer({
-      fromPubkey: user.publicKey,
-      toPubkey: new web3.PublicKey(
-        "Gqu7KLHnkdMrtfAN8TqQGU4o1PvXA2X4VTNkF4ok4t78"
-      ),
-      lamports: minRent,
-    }),
-  ]
+  // const instructions = [
+  //   web3.SystemProgram.transfer({
+  //     fromPubkey: user.publicKey,
+  //     toPubkey: new web3.PublicKey(
+  //       "Gqu7KLHnkdMrtfAN8TqQGU4o1PvXA2X4VTNkF4ok4t78"
+  //     ),
+  //     lamports: minRent,
+  //   }),
+  // ]
 
+  // const messageV0 = new web3.TransactionMessage({
+  //   payerKey: user.publicKey,
+  //   recentBlockhash: blockhash,
+  //   instructions,
+  // }).compileToV0Message()
+
+  // const transaction = new web3.VersionedTransaction(messageV0)
+
+  // transaction.sign([user])
+
+  // const txid = await connection.sendTransaction(transaction)
+  // console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`)
+
+  const slot = await connection.getSlot()
+
+  const [lookupTableInst, lookupTableAddress] =
+    web3.AddressLookupTableProgram.createLookupTable({
+      authority: user.publicKey,
+      payer: user.publicKey,
+      recentSlot: slot,
+    })
+
+  console.log("lookup table address:", lookupTableAddress.toBase58())
+
+  const instructions = [lookupTableInst]
+
+  // create v0 compatible message
   const messageV0 = new web3.TransactionMessage({
     payerKey: user.publicKey,
     recentBlockhash: blockhash,
@@ -30,68 +57,34 @@ async function main() {
 
   const transaction = new web3.VersionedTransaction(messageV0)
 
+  // sign your transaction with the required `Signers`
   transaction.sign([user])
 
   const txid = await connection.sendTransaction(transaction)
+  await connection.confirmTransaction(txid)
   console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`)
 
-  //   const slot = await connection.getSlot()
+  const extendInstruction = web3.AddressLookupTableProgram.extendLookupTable({
+    payer: user.publicKey,
+    authority: user.publicKey,
+    lookupTable: lookupTableAddress,
+    addresses: [user.publicKey],
+  })
 
-  //   // Assumption:
-  //   // `payer` is a valid `Keypair` with enough SOL to pay for the execution
+  const instructions2 = [extendInstruction]
 
-  //   const [lookupTableInst, lookupTableAddress] =
-  //     web3.AddressLookupTableProgram.createLookupTable({
-  //       authority: user.publicKey,
-  //       payer: user.publicKey,
-  //       recentSlot: slot,
-  //     })
+  const messageV02 = new web3.TransactionMessage({
+    payerKey: user.publicKey,
+    recentBlockhash: blockhash,
+    instructions: instructions2,
+  }).compileToV0Message()
 
-  //   console.log("lookup table address:", lookupTableAddress.toBase58())
+  const transaction2 = new web3.VersionedTransaction(messageV02)
 
-  //   // const instructions = [lookupTableInst]
+  transaction2.sign([user])
 
-  //   // // create v0 compatible message
-  //   // const messageV0 = new web3.TransactionMessage({
-  //   //   payerKey: user.publicKey,
-  //   //   recentBlockhash: blockhash,
-  //   //   instructions,
-  //   // }).compileToV0Message()
-
-  //   // const transaction = new web3.VersionedTransaction(messageV0)
-
-  //   // // sign your transaction with the required `Signers`
-  //   // transaction.sign([user])
-
-  //   // const txid = await connection.sendTransaction(transaction)
-  //   // console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`)
-
-  //   // add addresses to the `lookupTableAddress` table via an `extend` instruction
-  //   const extendInstruction = web3.AddressLookupTableProgram.extendLookupTable({
-  //     payer: user.publicKey,
-  //     authority: user.publicKey,
-  //     lookupTable: new web3.PublicKey(
-  //       "EVhGAyizGytvtMTmwC8d8anT4WsCp3131KzjknMYJxbP"
-  //     ),
-  //     addresses: [user.publicKey, web3.SystemProgram.programId],
-  //   })
-
-  //   const instructions = [extendInstruction]
-
-  //   // create v0 compatible message
-  //   const messageV0 = new web3.TransactionMessage({
-  //     payerKey: user.publicKey,
-  //     recentBlockhash: blockhash,
-  //     instructions,
-  //   }).compileToV0Message()
-
-  //   const transaction = new web3.VersionedTransaction(messageV0)
-
-  //   // sign your transaction with the required `Signers`
-  //   transaction.sign([user])
-
-  //   const txid = await connection.sendTransaction(transaction)
-  //   console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`)
+  const txid2 = await connection.sendTransaction(transaction2)
+  console.log(`https://explorer.solana.com/tx/${txid2}?cluster=devnet`)
 }
 
 main()
